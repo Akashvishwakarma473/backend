583bc91435b5aee2a9ed9e9c9ddb786eae8180e9
Akashvishwakarma473 <akashvishwakarma1212@gmail.com>
2024-02-08 15:55:31 +0530

    Make Other Changes

--- src/index.js
+++ src/index.js
@@ -5,6 +5,12 @@ dotenv.config({
     path: './env'
 })
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000)
+})
+.catch((err) => {
+    console.log("Mongo DB Connection Failed ! ", err);
+})
 
 
 
